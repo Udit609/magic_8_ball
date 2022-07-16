c004e2d2ba3fe26bbedace7331408a31684d3d8d
Udit <udit.sharma2021@vitbhopal.ac.in>
2022-07-16 17:47:53 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue.shade300,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: const Text('Ask Away'),
+        ),
+        body: const BallPage(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
